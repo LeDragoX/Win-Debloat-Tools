c04951307b0c13a87c8f2366063e21b5554be954
LeDragoX <plinio2xd@gmail.com>
2020-10-25 14:58:11 -0300

    fix backup-system.ps1

--- scripts/backup-system.ps1
+++ scripts/backup-system.ps1
@@ -1,5 +1,4 @@
 # Made by LeDragoX inspired by Chris Titus Tech
 Write-Output "Enabling system drive Restore Point"
-mkdir "$env:SystemDrive\WinBackup"
-Enable-ComputerRestore -Drive "$env:SystemDrive\WinBackup"
+Enable-ComputerRestore -Drive "$env:SystemDrive\" 
 Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
