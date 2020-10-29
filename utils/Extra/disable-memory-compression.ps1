c9033d314ebe5b1f3319d723cf7a7dc305aa9218
LeDragoX <plinio2xd@gmail.com>
2020-10-28 23:31:38 -0300

    echo to Write-Output

--- utils/Extra/disable-memory-compression.ps1
+++ utils/Extra/disable-memory-compression.ps1
@@ -2,5 +2,5 @@
 Disable-MMAgent -mc
 #Get-MMAgent
 
-echo "Now you can also disable service SysMain (former Superfetch) in case it's not used."
+Write-Output "Now you can also disable service SysMain (former Superfetch) in case it's not used."
 #Get-Service "SysMain" | Set-Service -StartupType Disabled -PassThru | Stop-Service
