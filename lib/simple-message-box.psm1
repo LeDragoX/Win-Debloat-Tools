3a14ae53daa9723283251ed4f8b83b3fe11499c4
LeDragoX <plinio2xd@gmail.com>
2020-12-16 12:19:32 -0300

    make fix-general-problems more dynamic

--- lib/simple-message-box.psm1
+++ lib/simple-message-box.psm1
@@ -1,6 +1,7 @@
+# Reference: https://michlstechblog.info/blog/powershell-show-a-messagebox/#:~:text=Sometimes%20while%20a%20powershell%20script,NET%20Windows.
 Function LoadSysForms {
     # Load assembly
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 }
 
 Function ShowMessage {
@@ -13,4 +14,18 @@ Function ShowMessage {
 
     LoadSysForms
     [System.Windows.Forms.MessageBox]::Show($Message, $Title, [System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Information)
+}
+
+function ShowQuestion {
+    param (
+        $Title = 'Insert title here',
+        [array]$Message = 
+        'Crash
+         Bandicoot'
+    )
+
+    LoadSysForms
+    $Answer = [System.Windows.Forms.MessageBox]::Show($Message, $Title, [System.Windows.Forms.MessageBoxButtons]::YesNoCancel,[System.Windows.Forms.MessageBoxIcon]::Question)
+
+    return $Answer
 }
