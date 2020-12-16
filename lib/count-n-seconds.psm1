428c67dd38ee8edaf22700eb7eae8679eb5bc6b3
LeDragoX <plinio2xd@gmail.com>
2020-12-16 16:35:43 -0300

    change the exiting script message

--- lib/count-n-seconds.psm1
+++ lib/count-n-seconds.psm1
@@ -1,7 +1,7 @@
 function CountNseconds {
     param (
         $Time = 3,
-        $Msg = 'Closing in'
+        $Msg = 'Exiting in'
     )
     $Time..0 | ForEach-Object {Start-Sleep -Seconds 1 ; "$Msg $_ seconds..."}
 }
