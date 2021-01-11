b6b6a4b00701b82f3e383faf391cb15dcd970b50
LeDragoX <plinio2xd@gmail.com>
2021-01-11 00:51:34 -0300

    Refactoring install-gaming-features.ps1

--- scripts/install-gaming-features.ps1
+++ scripts/install-gaming-features.ps1
@@ -1,10 +1,29 @@
 Write-Host "Install additional features for Windows..."
+Write-Host ""
 
-# Dism /online /Get-Features # To find all features
+# Dism /online /Get-Features #/Format:Table # To find all features
+# Get-WindowsOptionalFeature -Online
 
-Dism /Online /Enable-Feature /All /FeatureName:NetFx3
-Dism /Online /Enable-Feature /All /FeatureName:NetFx4-AdvSrvs
-Dism /Online /Enable-Feature /All /FeatureName:NetFx4Extended-ASPNET45
-Dism /Online /Enable-Feature /All /FeatureName:IIS-ASPNET
-Dism /Online /Enable-Feature /All /FeatureName:IIS-ASPNET45
-Dism /Online /Enable-Feature /All /FeatureName:DirectPlay
+$FeatureName = @(
+    "NetFx3"
+    "NetFx4-AdvSrvs"
+    "NetFx4Extended-ASPNET45"
+    "IIS-ASPNET"
+    "IIS-ASPNET45"
+    "DirectPlay"
+)
+
+foreach ($Feature in $FeatureName) {
+    $FeatureDetails = $(Get-WindowsOptionalFeature -Online -FeatureName $Feature)
+    
+    Write-Host "Checking if $Feature was already installed..."
+    Write-Host "$Feature is" $FeatureDetails.State
+    if ($FeatureDetails.State -like ("Enabled")) {
+        Write-Host "$Feature already installed! Skipping..."
+    }
+    elseif ($FeatureDetails.State -like "Disabled") {
+        Write-Host "Installing $Feature..."
+        Dism /Online /Enable-Feature /All /FeatureName:$Feature
+    }
+    Write-Host ""
+}
