8613af3acc4a57f70249bf237c807d6682b5c7fb
LeDragoX <plinio2xd@gmail.com>
2021-01-13 18:13:15 -0300

    No need to do that

--- DIY/enable-SysMain.bat
+++
@@ -1,6 +0,0 @@
-@echo This will enable SysMain/Superfetch
-
-Powershell "Get-Service -Name SysMain | Set-Service -StartupType Automatic"
-sc start SysMain
-
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnableSuperfetch /t REG_DWORD /d 3 /f
