acd4d774d1b4f790d928d4d4ff35dc15dde03b6b
LeDragoX <plinio2xd@gmail.com>
2021-01-14 19:18:20 -0300

    2/2 Fix #1 Issue

--- scripts/fix-general-problems.ps1
+++ scripts/fix-general-problems.ps1
@@ -1,4 +1,35 @@
-Write-Host "Original Folder $PSScriptRoot"
+Write-Host "Current Script Folder $PSScriptRoot"
+
+Write-Host "<==========================================>"
+Write-Host "          Resetting the Hosts file"
+Write-Host "<==========================================>"
+Write-Host ""
+
+$RestoreHosts = "# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by a '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
+
+# localhost name resolution is handled within DNS itself.
+#    127.0.0.1       localhost
+#    ::1             localhost"
+
+Push-Location "$env:SystemRoot\System32\drivers\etc"
+    Write-Output $RestoreHosts > .\hosts
+Pop-Location
 
 Write-Host "<==========================================>"
 Write-Host "            Resetting the MS Store"
