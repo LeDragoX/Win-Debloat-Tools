8f330576696b7faa1b03ccce3ac23f812a830150
LeDragoX <plinio2xd@gmail.com>
2021-01-15 15:44:47 -0300

    Change Arguments on WSL.msi file

--- scripts/all-in-one-tweaks.ps1
+++ scripts/all-in-one-tweaks.ps1
@@ -752,7 +752,7 @@ function EnableFeatures {
     # WSL 2 Setup Finish
     Push-Location "$env:SystemDrive\"
         Start-BitsTransfer -Source "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -Destination "wsl_update_x64.msi"
-        Start-Process .\wsl_update_x64.msi '/q /norestart' -Wait
+        Start-Process -FilePath ".\wsl_update_x64.msi" -ArgumentList "/q", "/norestart" -Wait
         Remove-Item "wsl_update_x64.msi" -Force
     Pop-Location
 
