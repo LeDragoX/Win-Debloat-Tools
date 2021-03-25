8cc559e2484a3b99a85871acfe5130dc587a7737
LeDragoX <plinio2xd@gmail.com>
2021-03-25 11:17:01 -0300

    Chocolatey will install CPU and GPU drivers separately

--- lib/Check-OS-Info.psm1
+++ lib/Check-OS-Info.psm1
@@ -8,43 +8,51 @@ function CheckOSArchitecture {
         Write-Host "64-bits OS detected!"
         $Architecture = "64-bits"
     } else {
-        Write-Host "Couldn't identify the System Architecture. :/"
-        $Architecture = "ARCH_NOT_FOUND (ARM?)"
-        break
+        Write-Host "ARCH_NOT_FOUND (ARM?) ... Couldn't identify the System Architecture. :/"
     }
 
-    # Note that $Architecture is not Global
     Write-Host "Architecture = $Architecture"
     return $Architecture
 }
 
+# Function to detect the current CPU
+function DetectCPU() {
+    # https://community.spiceworks.com/how_to/170332-how-to-get-cpu-information-in-windows-powershell
+    $Global:CPU = Get-WmiObject -Class Win32_Processor -ComputerName. | Select-Object -Property [a-z]*
+
+    if ($CPU.Name.contains("AMD") -or $CPU.Name.contains("Radeon")) {
+		Write-Host "AMD CPU found!"
+	} elseif ($CPU.Name.contains("Intel")) {
+        Write-Host "Intel CPU found!"
+	} else {
+        Write-Host "CPU_NOT_FOUND (NEW/CONFIDENTIAL?)"
+    }
+
+    Write-Host "CPU =" $CPU.Name
+    return $CPU.Name
+}
+
+
 # Function to detect the current GPU
-function DetectVideoCard() {
-	$Global:GPU
+function DetectGPU() {
 	# https://community.spiceworks.com/topic/1543645-powershell-get-wmiobject-win32_videocontroller-multiple-graphics-cards
 	$ArrComputers = "."
 
 	foreach ($Computer in $ArrComputers) {
-		$ComputerVideo = Get-WmiObject Win32_VideoController -ComputerName $Computer
-		Write-Host "Video Info: " $ComputerVideo.description
-		$Global:GPU = $ComputerVideo.description
+		$Global:GPU = Get-WmiObject -Class Win32_VideoController -ComputerName $Computer
+		Write-Host "Video Info:" $Global:GPU.description
     }
     
-    if ($GPU.contains("AMD") -or $GPU.contains("Radeon")) {
+    if ($GPU.description.contains("AMD") -or $GPU.description.contains("Radeon")) {
 		Write-Host "AMD GPU found!"
-        $GPU = "AMD"
-	} elseif ($GPU.contains("Intel")) {
+	} elseif ($GPU.description.contains("Intel")) {
         Write-Host "Intel GPU found!"
-        $GPU = "Intel"
-	} elseif ($GPU.contains("NVIDIA")) {
+	} elseif ($GPU.description.contains("NVIDIA")) {
         Write-Host "NVIDIA GPU found!"
-        $GPU = "NVIDIA"
     } else {
         Write-Host "GPU_NOT_FOUND (NEW/CONFIDENTIAL?)"
-        $GPU = "GPU_NOT_FOUND (NEW/CONFIDENTIAL?)"
     }
 
-    # Note that $GPU is not Global
-    Write-Host "GPU = $GPU"
-    return $GPU
+    Write-Host "GPU =" $GPU.description
+    return $GPU.description
 }
