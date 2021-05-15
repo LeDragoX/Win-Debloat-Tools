ede56c896775902543508df1aa6c67b4af22d780
LeDragoX <plinio2xd@gmail.com>
2021-05-15 12:51:58 -0300

    Even if SysMain is not great, Memory Compression still

--- utils/disable-memory-compression.ps1
+++
@@ -1,6 +0,0 @@
-# disable Memory Compression (requires SysMain (service))
-Disable-MMAgent -mc
-#Get-MMAgent
-
-Write-Output "Now you can also disable service SysMain (former Superfetch) in case it's not used."
-#Get-Service "SysMain" | Set-Service -StartupType Disabled -PassThru | Stop-Service
