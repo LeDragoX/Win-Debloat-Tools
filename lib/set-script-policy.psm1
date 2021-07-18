dbc021a0934311b15dc439f0d5aeaafa02907d94
LeDragoX <plinio2xd@gmail.com>
2021-07-18 18:08:52 -0300

    Remove "Process" scope

--- lib/set-script-policy.psm1
+++ lib/set-script-policy.psm1
@@ -1,9 +1,8 @@
 Function UnrestrictPermissions {
     Write-Host "[+] Receiving permissions to run scripts"
     Write-Host "" # Skip Line
-    Set-ExecutionPolicy Unrestricted -Scope Process -Force
-    Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
     Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+    Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
     Get-ExecutionPolicy -List
     Write-Host "" # Skip Line
 }
@@ -11,9 +10,8 @@ Function UnrestrictPermissions {
 Function RestrictPermissions {
     Write-Host "[-] Denying permissions to run scripts"
     Write-Host "" # Skip Line
-    Set-ExecutionPolicy Restricted -Scope Process -Force
-    Set-ExecutionPolicy Restricted -Scope CurrentUser -Force
-    Set-ExecutionPolicy Restricted -Scope LocalMachine -Force
+    Set-ExecutionPolicy Restricted -Scope LocalMachine -Force   # Reminds HKLM
+    Set-ExecutionPolicy Restricted -Scope CurrentUser -Force    # Reminds HKCU
     Get-ExecutionPolicy -List
     Write-Host "" # Skip Line
 }
