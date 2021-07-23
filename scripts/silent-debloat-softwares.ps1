a6b9a5ec6e82f301cd360dd4d4c7effcf8f91e95
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-23 01:51:14 -0300

    Refactor on downloading

--- scripts/silent-debloat-softwares.ps1
+++ scripts/silent-debloat-softwares.ps1
@@ -2,24 +2,28 @@
 
 Function RunDebloatSoftwares() {
         
-    # If changing the programs folder move here!!!
-    Push-Location -Path "..\lib\debloat-softwares\"
+    if (!(Test-Path "$PSScriptRoot\..\tmp")) {
+        Write-Host "[+] Folder $PSScriptRoot\..\tmp doesn't exist, creating..."
+        mkdir "$PSScriptRoot\..\tmp" | Out-Null
+    }
 
-    Write-Host "[+] Running ShutUp10 and applying Recommended settings..."
-
-    Push-Location -Path "ShutUp10\"
-
-    Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile "OOSU10.exe"
-    Start-Process -FilePath ".\OOSU10.exe" -ArgumentList "ooshutup10.cfg", "/quiet" -Wait   # Wait until the process closes
-    Remove-Item "*.*" -Exclude "*.cfg" -Force                                               # Leave no traces
-    
-    Pop-Location
+    Push-Location -Path "$PSScriptRoot\..\tmp"
 
     Write-Host "[+] Running AdwCleaner to do a Quick Virus/Adware Scan..."
     Invoke-WebRequest -Uri "https://downloads.malwarebytes.com/file/adwcleaner" -OutFile "adwcleaner.exe"
     Start-Process -FilePath ".\adwcleaner.exe" -ArgumentList "/eula", "/clean", "/noreboot" -Wait
     Remove-Item ".\adwcleaner.exe" -Force
 
+    Pop-Location
+    Push-Location -Path "$PSScriptRoot\..\lib\debloat-softwares\"
+    Push-Location -Path "ShutUp10\"
+    
+    Write-Host "[+] Running ShutUp10 and applying Recommended settings..."
+    Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile "OOSU10.exe"
+    Start-Process -FilePath ".\OOSU10.exe" -ArgumentList "ooshutup10.cfg", "/quiet" -Wait   # Wait until the process closes
+    Remove-Item "*.*" -Exclude "*.cfg" -Force                                               # Leave no traces
+    
+    Pop-Location
     Pop-Location
     
 }
