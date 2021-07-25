e7ee6ffc7b71645e5722de04e7253d71b3951805
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-25 15:02:21 -0300

    Remove 2nd NVIDIA driver install and install it correctly

--- src/scripts/software-installer.ps1
+++ src/scripts/software-installer.ps1
@@ -16,6 +16,7 @@ function InstallPackages() {
         Section1 -Text "Installing AMD chipset drivers!"
         If ($CPU.contains("Ryzen")) {
             Section1 -Text "You have a Ryzen CPU, installing Chipset driver for Ryzen processors!"
+            Caption1 -Text "Installing: amd-ryzen-chipset"
             choco install -y "amd-ryzen-chipset"                # AMD Ryzen Chipset Drivers
         }
         
@@ -23,8 +24,14 @@ function InstallPackages() {
     ElseIf ($CPU.contains("Intel")) {
 
         Section1 -Text "Installing Intel chipset drivers!"
+
+        Caption1 -Text "Installing: chocolatey-misc-helpers.extension"
         choco install -y "chocolatey-misc-helpers.extension"    # Chocolatey Misc Helpers Extension ('intel-dsa' Dependency)
+        
+        Caption1 -Text "Installing: dotnet4.7"
         choco install -y "dotnet4.7"                            # Microsoft .NET Framework 4.7 ('intel-dsa' Dependency)
+        
+        Caption1 -Text "Installing: intel-dsa"
         choco install -y "intel-dsa"                            # Intel® Driver & Support Assistant (Intel® DSA)
 
     }
@@ -36,16 +43,20 @@ function InstallPackages() {
     
     If ($GPU.contains("Intel")) {
         Section1 -Text "Installing Intel Graphics driver!"
+        Caption1 -Text "Installing: intel-graphics-driver"
         choco install -y "intel-graphics-driver"                # Intel Graphics Driver (latest)
     }
 
     If ($GPU.contains("NVIDIA")) {
 
         Section1 -Text "Installing NVIDIA Graphics driver!"
+        Caption1 -Text "Installing: geforce-experience"
         choco install -y "geforce-experience"                   # GeForce Experience (latest)
+
+        Caption1 -Text "Configuring 'geforce-game-ready-driver' for DCH..."
         choco feature enable -n=useRememberedArgumentsForUpgrades
-        cinst geforce-game-ready-driver --package-parameters="'/dch'"
-        choco install -y "geforce-game-ready-driver"            # GeForce Game Ready Driver (latest)
+        Caption1 -Text "Installing: geforce-game-ready-driver"
+        choco install -y "geforce-game-ready-driver" --package-parameters="'/dch'" # GeForce Game Ready Driver (latest)
 
     }
 
