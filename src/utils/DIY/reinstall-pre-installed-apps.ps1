855b6dfd309ef57d053ace528f8c4e5137e5363a
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-26 15:33:51 -0300

    Fix file requirements

--- src/utils/DIY/reinstall-pre-installed-apps.ps1
+++ src/utils/DIY/reinstall-pre-installed-apps.ps1
@@ -1,22 +1,29 @@
+function QuickPrivilegesElevation() {
+	# Used from https://stackoverflow.com/a/31602095 because it preserves the working directory!
+	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+}
+
 function Main() {
+
+	QuickPrivilegesElevation
 	
 	# Get all the provisioned packages
-	$Packages = (get-item 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Applications') | Get-ChildItem
+	$Packages = (Get-Item 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Applications') | Get-ChildItem
 
 	# Filter the list if provided a filter
 	$PackageFilter = $args[0]
 	if ([string]::IsNullOrEmpty($PackageFilter)) {
-		Write-Output "No filter specified, attempting to re-register all provisioned apps."
+		Write-Host "No filter specified, attempting to re-register all provisioned apps."
 	}
 	else {
 		$Packages = $Packages | Where-Object { $_.Name -like $PackageFilter } 
 
 		if ($null -eq $Packages) {
-			Write-Output "No provisioned apps match the specified filter."
+			Write-Host "No provisioned apps match the specified filter."
 			exit
 		}
 		else {
-			Write-Output "Registering the provisioned apps that match $PackageFilter"
+			Write-Host "Registering the provisioned apps that match $PackageFilter"
 		}
 	}
 
@@ -26,7 +33,7 @@ function Main() {
 		$PackagePath = [System.Environment]::ExpandEnvironmentVariables(($Package | Get-ItemProperty | Select-Object -ExpandProperty Path))
 
 		# register the package	
-		Write-Output "Attempting to register package: $PackageName"
+		Write-Host "Attempting to register package: $PackageName"
 
 		Add-AppxPackage -register $PackagePath -DisableDevelopmentMode
 	}
