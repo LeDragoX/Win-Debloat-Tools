566632b610f6e492e51f92226c96136a447b63a0
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-30 16:25:37 -0300

    Remove switching font encoding to UTF-8 - Removing small test documentation

--- src/lib/setup-console-style.psm1
+++ src/lib/setup-console-style.psm1
@@ -1,17 +1,6 @@
-<#
-.SYNOPSIS
-    Accept accents and console color to green
-.DESCRIPTION
-    Accept font accents and change the console color to green
-.EXAMPLE
-    PS C:\> Import-Module .\{FilePath}\setup-console-style.psm1
-    PS C:\> SetupConsoleStyle
-    Accept font accents and change the console color to green 
-#>
 Function SetupConsoleStyle() {
     [CmdletBinding()] #<<-- This turns a regular function into an advanced function
     param ()
 
-    chcp 65001
     cmd /c color A
 }
