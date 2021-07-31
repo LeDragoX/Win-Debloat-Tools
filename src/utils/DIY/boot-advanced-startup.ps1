6100620dac431dd368e2a1ac7d40eabd87348f08
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-31 17:06:57 -0300

    Put everything inside the Main function

--- src/utils/DIY/boot-advanced-startup.ps1
+++ src/utils/DIY/boot-advanced-startup.ps1
@@ -1,5 +1,11 @@
-# -r: Complete shutdown and restart
-# -o: Reboot into the advanced menu
-# -t: Period before shutdown
-# -f: Force shutdown
-shutdown -o -r -f -t 0
+function Main() {
+
+  # -r: Complete shutdown and restart
+  # -o: Reboot into the advanced menu
+  # -t: Period before shutdown
+  # -f: Force shutdown
+  shutdown -o -r -f -t 0
+
+}
+
+Main
