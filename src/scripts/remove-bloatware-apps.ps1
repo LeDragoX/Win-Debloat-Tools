ddd3c32c8342dc7e7ca24e0c510f666fb7a2c08e
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-31 16:17:52 -0300

    Remove apps from all users | Remove Xbox Apps - Insert comments on some apps

--- src/scripts/remove-bloatware-apps.ps1
+++ src/scripts/remove-bloatware-apps.ps1
@@ -6,63 +6,71 @@ Function RemoveBloatwareApps() {
 
     $Apps = @(
         # [Alphabetic order] Default Windows 10 apps
-        "Microsoft.3DBuilder"
+        "Microsoft.3DBuilder"                       # 3D Builder
         "Microsoft.Appconnector"
-        "Microsoft.BingFinance"
-        "Microsoft.BingFoodAndDrink"
-        "Microsoft.BingHealthAndFitness"
-        "Microsoft.BingNews"
-        "Microsoft.BingSports"
-        "Microsoft.BingTranslator"
-        "Microsoft.BingTravel"
-        "Microsoft.BingWeather"
+        "Microsoft.BingFinance"                     # Finance
+        "Microsoft.BingFoodAndDrink"                # Food And Drink
+        "Microsoft.BingHealthAndFitness"            # Health And Fitness
+        "Microsoft.BingNews"                        # News
+        "Microsoft.BingSports"                      # Sports
+        "Microsoft.BingTranslator"                  # Translator
+        "Microsoft.BingTravel"                      # Travel
+        "Microsoft.BingWeather"                     # Weather
         "Microsoft.CommsPhone"
         "Microsoft.ConnectivityStore"
+        "Microsoft.GamingServices"
         "Microsoft.GetHelp"
         "Microsoft.Getstarted"
         "Microsoft.Messaging"
         "Microsoft.Microsoft3DViewer"
         "Microsoft.MicrosoftOfficeHub"
         "Microsoft.MicrosoftPowerBIForWindows"
-        "Microsoft.MicrosoftSolitaireCollection"
-        "Microsoft.MinecraftUWP"
+        "Microsoft.MicrosoftSolitaireCollection"    # MS Solitaire
         "Microsoft.MixedReality.Portal"
         "Microsoft.NetworkSpeedTest"
-        "Microsoft.Office.OneNote"
+        "Microsoft.Office.OneNote"                  # MS Office One Note
         "Microsoft.Office.Sway"
         "Microsoft.OneConnect"
-        "Microsoft.People"
-        "Microsoft.Print3D"
+        "Microsoft.People"                          # People
+        "Microsoft.MSPaint"                         # Paint 3D (Where every artist truly start as a kid, i mean, on original Paint, not this 3D)
+        "Microsoft.Print3D"                         # Print 3D
         "Microsoft.ScreenSketch"
-        "Microsoft.SkypeApp"                        # Who still uses Skype? Use Discord
+        "Microsoft.SkypeApp"                        # Skype (Who still uses Skype? Use Discord)
+        "Microsoft.Todos"                           # Microsoft To Do
         "Microsoft.Wallet"
-        "Microsoft.WindowsAlarms"
+        "Microsoft.WindowsAlarms"                   # Alarms
         "microsoft.windowscommunicationsapps"
-        "Microsoft.WindowsMaps"
+        "Microsoft.WindowsMaps"                     # Maps
         "Microsoft.WindowsPhone"
         "Microsoft.WindowsReadingList"
         "Microsoft.WindowsSoundRecorder"
-        "Microsoft.YourPhone"
+        "Microsoft.XboxApp"                         # Xbox Console Companion (Replaced by new App)
+        "Microsoft.XboxGameCallableUI"
+        "Microsoft.XboxGameOverlay"
+        "Microsoft.XboxIdentityProvider"            # Xbox Dependency
+        "Microsoft.XboxSpeechToTextOverlay"
+        "Microsoft.Xbox.TCUI"
+        "Microsoft.YourPhone"                       # Your Phone
         "Microsoft.ZuneMusic"
         "Microsoft.ZuneVideo"
         
         # [Alphabetic order] 3rd party Apps
         "*ACGMediaPlayer*"
         "*ActiproSoftwareLLC*"
-        "*AdobePhotoshopExpress*"
-        "*Asphalt8Airborne*"
+        "*AdobePhotoshopExpress*"                   # Adobe Photoshop Express
+        "*Asphalt8Airborne*"                        # Asphalt 8 Airbone
         "*AutodeskSketchBook*"
-        "*BubbleWitch3Saga*"
+        "*BubbleWitch3Saga*"                        # Bubble Witch 3 Saga
         "*CaesarsSlotsFreeCasino*"
-        "*CandyCrush*"
+        "*CandyCrush*"                              # Candy Crush
         "*COOKINGFEVER*"
         "*CyberLinkMediaSuiteEssentials*"
         "*DisneyMagicKingdoms*"
-        "*Dolby*"
+        "*Dolby*"                                   # Dolby Products (Like Atmos)
         "*DrawboardPDF*"
-        "*Duolingo-LearnLanguagesforFree*"
+        "*Duolingo-LearnLanguagesforFree*"          # Duolingo
         "*EclipseManager*"
-        "*Facebook*"
+        "*Facebook*"                                # Facebook
         "*FarmVille2CountryEscape*"
         "*FitbitCoach*"
         "*Flipboard*"
@@ -77,15 +85,15 @@ Function RemoveBloatwareApps() {
         "*PandoraMediaInc*"
         "*PhototasticCollage*"
         "*PicsArt-PhotoStudio*"
-        "*Plex*"  
+        "*Plex*"
         "*PolarrPhotoEditorAcademicEdition*"
-        "*RoyalRevolt*"
+        "*RoyalRevolt*"                         # Royal Revolt
         "*Shazam*"
         "*SlingTV*"
         "*Speed Test*"
         "*Sway*"
         "*TuneInRadio*"
-        "*Twitter*"
+        "*Twitter*"                             # Twitter
         "*Viber*"
         "*WinZipUniversal*"
         "*Wunderlist*"
@@ -99,24 +107,15 @@ Function RemoveBloatwareApps() {
         # [DIY] Default apps i'll keep
 
         #"Microsoft.FreshPaint"
-        #"Microsoft.GamingServices"
-        #"Microsoft.MicrosoftEdge"
-        #"Microsoft.MicrosoftStickyNotes"           # Productivity
-        #"Microsoft.MSPaint"                        # Where every artist truly start as a kid
-        #"Microsoft.WindowsCalculator"              # A basic need
-        #"Microsoft.WindowsCamera"                  # People may use it
-        #"Microsoft.Windows.Photos"                 # Reproduce GIFs
+        #"Microsoft.MicrosoftEdge"          # This browser finally became something
+        #"Microsoft.MicrosoftStickyNotes"   # Productivity
+        #"Microsoft.WindowsCalculator"      # How much is (98357489.253 x 5347658.845937) / 924 ?
+        #"Microsoft.WindowsCamera"          # Keep to test Camera
+        #"Microsoft.Windows.Photos"         # Reproduce GIFs
         
         # [DIY] Xbox Apps and Dependencies
         
-        #"Microsoft.Xbox.TCUI"
-        #"Microsoft.XboxApp"
-        #"Microsoft.XboxGameOverlay"
-        #"Microsoft.XboxGamingOverlay"
-        #"Microsoft.XboxSpeechToTextOverlay"
-        # Apps which cannot be removed using Remove-AppxPackage from Xbox
-        #"Microsoft.XboxGameCallableUI"
-        #"Microsoft.XboxIdentityProvider"
+        #"Microsoft.XboxGamingOverlay"      # Xbox Game Bar
         
         # [DIY] Common Streaming services
         
@@ -137,8 +136,8 @@ Function RemoveBloatwareApps() {
 
     ForEach ($Bloat in $Apps) {
         Write-Host "[-][UWP] Trying to remove $Bloat ..."
-        Get-AppxPackage -Name $Bloat | Remove-AppxPackage    # App
-        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Bloat | Remove-AppxProvisionedPackage -Online  # Payload
+        Get-AppxPackage -AllUsers -Name $Bloat | Remove-AppxPackage    # App
+        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Bloat | Remove-AppxProvisionedPackage -Online -AllUsers  # Payload
     }
 
 }
