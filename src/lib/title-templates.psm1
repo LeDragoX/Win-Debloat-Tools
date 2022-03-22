fa37ad7a6cff2b49ba7453ce8fdb0df4988faec1
Plínio Larrubia <plinio2xd@gmail.com>
2022-03-22 14:23:45 -0300

    Change Script Art on the beginning

--- src/lib/title-templates.psm1
+++ src/lib/title-templates.psm1
@@ -58,20 +58,27 @@ function Write-ASCIIScriptName() {
     [CmdletBinding()] #<<-- This turns a regular function into an advanced function
     param ()
 
-    "
-    
-    "
-    Write-Host '   __        ___         _  ___          ____                       _   ' -ForegroundColor Cyan
-    Write-Host '   \ \      / (_)_ __   / |/ _ \   _    / ___| _ __ ___   __ _ _ __| |_ ' -ForegroundColor Cyan
-    Write-Host '    \ \ /\ / /| | "_ \  | | | | |_| |_  \___ \| "_ ` _ \ / _` | "__| __|' -ForegroundColor Cyan
-    Write-Host '     \ V  V / | | | | | | | |_| |_   _|  ___) | | | | | | (_| | |  | |_ ' -ForegroundColor Cyan
-    Write-Host '      \_/\_/  |_|_| |_| |_|\___/  |_|   |____/|_| |_| |_|\__,_|_|   \__|' -ForegroundColor Cyan
-    Write-Host '                                                                        ' -ForegroundColor Cyan
-    Write-Host '        ____       _     _             _     _____           _          ' -ForegroundColor Cyan
-    Write-Host '       |  _ \  ___| |__ | | ___   __ _| |_  |_   _|__   ___ | |___      ' -ForegroundColor Cyan
-    Write-Host '       | | | |/ _ \ "_ \| |/ _ \ / _` | __|   | |/ _ \ / _ \| / __|     ' -ForegroundColor Cyan
-    Write-Host '       | |_| |  __/ |_) | | (_) | (_| | |_    | | (_) | (_) | \__ \     ' -ForegroundColor Cyan
-    Write-Host "       |____/ \___|_.__/|_|\___/ \__,_|\__|   |_|\___/ \___/|_|___/ `n`n" -ForegroundColor Cyan
+    Write-Host "<=========================================================================================================>`n" -ForegroundColor White
+    Write-Host '888       888 d8b           d888   .d8888b.                .d8888b.                                 888    ' -ForegroundColor Green
+    Write-Host '888   o   888 Y8P          d8888  d88P  Y88b              d88P  Y88b                                888    ' -ForegroundColor Green
+    Write-Host '888  d8b  888                888  888    888              Y88b.                                     888    ' -ForegroundColor Green
+    Write-Host '888 d888b 888 888 88888b.    888  888    888   888         "Y888b.   88888b.d88b.   8888b.  888d888 888888 ' -ForegroundColor Green
+    Write-Host '888d88888b888 888 888 "88b   888  888    888 8888888          "Y88b. 888 "888 "88b     "88b 888P"   888    ' -ForegroundColor Green
+    Write-Host '88888P Y88888 888 888  888   888  888    888   888              "888 888  888  888 .d888888 888     888    ' -ForegroundColor Green
+    Write-Host '8888P   Y8888 888 888  888   888  Y88b  d88P              Y88b  d88P 888  888  888 888  888 888     Y88b.  ' -ForegroundColor Green
+    Write-Host '888P     Y888 888 888  888 8888888 "Y8888P"                "Y8888P"  888  888  888 "Y888888 888      "Y888 ' -ForegroundColor Green
+    Write-Host '                                                                                                           ' -ForegroundColor Green
+    Write-Host '  8888888b.           888      888                   888         88888888888                888            ' -ForegroundColor Green
+    Write-Host '  888  "Y88b          888      888                   888             888                    888            ' -ForegroundColor Green
+    Write-Host '  888    888          888      888                   888             888                    888            ' -ForegroundColor Green
+    Write-Host '  888    888  .d88b.  88888b.  888  .d88b.   8888b.  888888          888   .d88b.   .d88b.  888 .d8888b    ' -ForegroundColor Green
+    Write-Host '  888    888 d8P  Y8b 888 "88b 888 d88""88b     "88b 888             888  d88""88b d88""88b 888 88K        ' -ForegroundColor Green
+    Write-Host '  888    888 88888888 888  888 888 888  888 .d888888 888             888  888  888 888  888 888 "Y8888b.   ' -ForegroundColor Green
+    Write-Host '  888  .d88P Y8b.     888 d88P 888 Y88..88P 888  888 Y88b.           888  Y88..88P Y88..88P 888      X88   ' -ForegroundColor Green
+    Write-Host "  8888888P'   'Y8888  88888P'  888  'Y88P'  'Y888888  'Y888          888   'Y88P'   'Y88P'  888  88888P'   `n" -ForegroundColor Green    
+    Write-Host "<=========================================================================================================>" -ForegroundColor White
+    Write-Host "                                        It's Time to Debloat Windows" -ForegroundColor Green
+    Write-Host "<=========================================================================================================>" -ForegroundColor White
 
 }
 
