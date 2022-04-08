271d318a0f5fc801ae544a970f52da992929d311
Plínio Larrubia <plinio2xd@gmail.com>
2022-04-08 14:48:57 -0300

    Keep WinSAT enabled, for good reasons (Thanks Baboo!)

--- src/scripts/optimize-scheduled-tasks.ps1
+++ src/scripts/optimize-scheduled-tasks.ps1
@@ -35,7 +35,7 @@ function Optimize-ScheduledTasksList() {
     Write-Title -Text "Scheduled Tasks tweaks"
     Write-Section -Text "Disabling Scheduled Tasks"
 
-    # Took from: https://docs.microsoft.com/pt-br/windows-server/remote/remote-desktop-services/rds-vdi-recommendations#task-scheduler
+    # Adapted from: https://docs.microsoft.com/pt-br/windows-server/remote/remote-desktop-services/rds-vdi-recommendations#task-scheduler
     $DisableScheduledTasks = @(
         "\Microsoft\Office\OfficeTelemetryAgentLogOn"
         "\Microsoft\Office\OfficeTelemetryAgentFallBack"
@@ -44,24 +44,23 @@ function Optimize-ScheduledTasksList() {
         "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
         "\Microsoft\Windows\Application Experience\StartupAppTask"
         "\Microsoft\Windows\Autochk\Proxy"
-        "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"           # Recommended state for VDI use
-        "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask"         # Recommended state for VDI use
+        "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"         # Recommended state for VDI use
+        "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask"       # Recommended state for VDI use
         "\Microsoft\Windows\Customer Experience Improvement Program\Uploader"
-        "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"                # Recommended state for VDI use
-        "\Microsoft\Windows\Defrag\ScheduledDefrag"                                         # Recommended state for VDI use
+        "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"              # Recommended state for VDI use
+        "\Microsoft\Windows\Defrag\ScheduledDefrag"                                       # Recommended state for VDI use
         "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector"
-        "\Microsoft\Windows\Location\Notifications"                                         # Recommended state for VDI use
-        "\Microsoft\Windows\Location\WindowsActionDialog"                                   # Recommended state for VDI use
-        "\Microsoft\Windows\Maintenance\WinSAT"                                             # Recommended state for VDI use
-        "\Microsoft\Windows\Maps\MapsToastTask"                                             # Recommended state for VDI use
-        "\Microsoft\Windows\Maps\MapsUpdateTask"                                            # Recommended state for VDI use
-        "\Microsoft\Windows\Mobile Broadband Accounts\MNO Metadata Parser"                  # Recommended state for VDI use
-        "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem"                     # Recommended state for VDI use
-        "\Microsoft\Windows\Retail Demo\CleanupOfflineContent"                              # Recommended state for VDI use
-        "\Microsoft\Windows\Shell\FamilySafetyMonitor"                                      # Recommended state for VDI use
-        "\Microsoft\Windows\Shell\FamilySafetyRefreshTask"                                  # Recommended state for VDI use
+        "\Microsoft\Windows\Location\Notifications"                                       # Recommended state for VDI use
+        "\Microsoft\Windows\Location\WindowsActionDialog"                                 # Recommended state for VDI use
+        "\Microsoft\Windows\Maps\MapsToastTask"                                           # Recommended state for VDI use
+        "\Microsoft\Windows\Maps\MapsUpdateTask"                                          # Recommended state for VDI use
+        "\Microsoft\Windows\Mobile Broadband Accounts\MNO Metadata Parser"                # Recommended state for VDI use
+        "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem"                   # Recommended state for VDI use
+        "\Microsoft\Windows\Retail Demo\CleanupOfflineContent"                            # Recommended state for VDI use
+        "\Microsoft\Windows\Shell\FamilySafetyMonitor"                                    # Recommended state for VDI use
+        "\Microsoft\Windows\Shell\FamilySafetyRefreshTask"                                # Recommended state for VDI use
         "\Microsoft\Windows\Shell\FamilySafetyUpload"
-        "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary"                            # Recommended state for VDI use
+        "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary"                          # Recommended state for VDI use
     )
 
     ForEach ($ScheduledTask in $DisableScheduledTasks) {
@@ -76,8 +75,9 @@ function Optimize-ScheduledTasksList() {
 
     Write-Section -Text "Enabling Scheduled Tasks"
     $EnableScheduledTasks = @(
-        "\Microsoft\Windows\RecoveryEnvironment\VerifyWinRE"            # It's about the Recovery before starting Windows, with Diagnostic tools and Troubleshooting when your PC isn't healthy, need this ON.
-        "\Microsoft\Windows\Windows Error Reporting\QueueReporting"     # Windows Error Reporting event, needed to improve compatibility with your hardware
+        "\Microsoft\Windows\Maintenance\WinSAT"                     # WinSAT detects incorrect system configurations, that causes performance loss, then sends it via telemetry | Reference (PT-BR): https://youtu.be/wN1I0IPgp6U?t=16
+        "\Microsoft\Windows\RecoveryEnvironment\VerifyWinRE"        # It's about the Recovery before starting Windows, with Diagnostic tools and Troubleshooting when your PC isn't healthy, need this ON.
+        "\Microsoft\Windows\Windows Error Reporting\QueueReporting" # Windows Error Reporting event, needed to improve compatibility with your hardware
     )
 
     ForEach ($ScheduledTask in $EnableScheduledTasks) {
