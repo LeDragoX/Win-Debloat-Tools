542afb5b9f987fb64e41fd206068999779f2d396
Plínio Larrubia <plinio2xd@gmail.com>
2022-05-03 14:27:24 -0300

    feat: Install Nerd Fonts is isolated in as a new option
    - Install JetBrains Mono font aside with MesloLGS
    - Install-Font lib updated to install TrueType or OpenType fonts
    - Removed verification before installing fonts, it was a soft and useless way to verify
    - Fonts folder will be deleted after process
    
    feat: Windows Terminal will remain as first option on Devlopment (Windows)
    
    fix: GUI elements with wrong button sizes
    
    - Also put yellow buttons and blue checkboxes in Italic style
    
    fix: Put src/tmp/Fonts folder on .gitignore

--- src/lib/install-font.psm1
+++ src/lib/install-font.psm1
@@ -1,33 +1,52 @@
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\"title-templates.psm1"
+
 # Adapted From: https://gist.github.com/anthonyeden/0088b07de8951403a643a8485af2709b?permalink_comment_id=3651336#gistcomment-3651336
 
 function Install-Font() {
-  [CmdletBinding()]
-  param (
-    [String] $FontSourceFolder = "$PSScriptRoot\src\tmp\Fonts"
-  )
+    [CmdletBinding()]
+    param (
+        [String] $FontSourceFolder = "$PSScriptRoot\src\tmp\Fonts"
+    )
 
-  $SystemFontsPath = "$env:SystemRoot\Fonts"
+    $PathToLMWindowsFonts = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+    $SystemFontsPath = "$env:SystemRoot\Fonts"
 
-  ForEach ($FontFile in Get-ChildItem $FontSourceFolder -Include '*.ttf', '*.ttc', '*.otf' -Recurse) {
-    $TargetPath = Join-Path $SystemFontsPath $FontFile.Name
+    ForEach ($FontFile in Get-ChildItem $FontSourceFolder -Include '*.ttf', '*.ttc', '*.otf' -Recurse) {
+        $TargetPath = Join-Path $SystemFontsPath $FontFile.Name
 
-    If (!(Test-Path "$TargetPath")) {
-      Write-Host "[+] Installing '$($FontFile.Name)' Font on $TargetPath..."
+        Write-Status -Symbol "+" -Status "Installing '$($FontFile.Name)' Font on $TargetPath ..."
 
-      # Extract Font information for Reqistry
-      $ShellFolder = (New-Object -COMObject Shell.Application).Namespace($FontSourceFolder)
-      $ShellFile = $ShellFolder.ParseName($FontFile.Name)
-      $ShellFileType = $ShellFolder.GetDetailsOf($ShellFile, 2)
-      If ($ShellFileType -Like '*TrueType font file*') { $FontType = '(TrueType)' }
+        Try {
+            # Extract Font information for Reqistry
+            $ShellFolder = (New-Object -COMObject Shell.Application).Namespace($FontSourceFolder)
+            $ShellFile = $ShellFolder.ParseName($FontFile.Name)
+            $ShellFileType = $ShellFolder.GetDetailsOf($ShellFile, 2)
+            If ($ShellFileType -Like '*TrueType font file*') { $FontType = '(TrueType)' }
 
-      # Update Registry and copy Font to Font directory
-      $RegName = $ShellFolder.GetDetailsOf($ShellFile, 21) + ' ' + $FontType
-      New-ItemProperty -Name $RegName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType String -Value $FontFile.Name -Force
-      Copy-item $FontFile.FullName -Destination $SystemFontsPath
-      Remove-Item $FontFile.FullName
+            # Update Registry and copy Font to Font directory
+            $RegName = $ShellFolder.GetDetailsOf($ShellFile, 21) + ' ' + $FontType
+        }
+        Catch {
+            # This may not be the better way, but this workaround worked
+            Write-Status -Symbol "@" -Status "Got an error, the font type is OpenType" -Warning
+            $FontType = '(OpenType)'
+            $RegName = ""
+            $NameHelper = $FontFile.Name.Replace("-", "").Replace(".ttf", "").Replace(".ttc", "").Replace(".otf", "").Trim(" ")
+            $NameHelper = $NameHelper.Replace("Mono", " Mono").Replace("NF", " NF").Replace("NL", " NL").Trim(" ")
+            $NameHelper = $NameHelper.Replace("Thin", " Thin").Replace("Semi", " Semi").Replace("Medium", " Medium").Replace("Extra", " Extra").Trim(" ")
+            $NameHelper = $NameHelper.Replace("Bold", " Bold").Replace("Italic", " Italic").Replace("Light", " Light").Replace("Regular", " Regular").Trim(" ")
+            $NameHelper = $NameHelper + " " + $FontType
+
+            ForEach ($Item in $NameHelper.Split(" ")) {
+                If (($Item -ne " ") -or ($null -ne $Item)) {
+                    $RegName = $RegName.Trim(" ") + " " + $Item.Trim(" ")
+                }
+            }
+        }
+
+        Write-Status -Symbol "+" -Status "Creating new Registry to $RegName on: $PathToLMWindowsFonts"
+        New-ItemProperty -Path "$PathToLMWindowsFonts" -Name $RegName -PropertyType String -Value $FontFile.Name -Force
+        Copy-item $FontFile.FullName -Destination $SystemFontsPath
+        Remove-Item $FontFile.FullName
     }
-    ElseIf (Test-Path "$TargetPath") {
-      Write-Host "[?] $($FontFile.Name) is already installed!" -ForegroundColor Yellow -BackgroundColor Black
-    }
-  }
 }
