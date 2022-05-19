4f17107bdbb31d8ee1e09000227bf068af0d9a84
Plínio Larrubia <plinio2xd@gmail.com>
2022-05-19 14:49:42 -0300

    `misc/refactor`: rename optimize-scheduled-tasks to optimize-task-scheduler

--- Win10ScriptGUI.ps1
+++ Win10ScriptGUI.ps1
@@ -656,7 +656,7 @@ function Show-GUI() {
                 # [Recommended order]
                 "backup-system.ps1",
                 "silent-debloat-softwares.ps1",
-                "optimize-scheduled-tasks.ps1",
+                "optimize-task-scheduler.ps1",
                 "optimize-services.ps1",
                 "remove-bloatware-apps.ps1",
                 "optimize-privacy.ps1",
@@ -676,7 +676,7 @@ function Show-GUI() {
     $UndoTweaks.Add_Click( {
             $Global:Revert = $true
             $Scripts = @(
-                "optimize-scheduled-tasks.ps1",
+                "optimize-task-scheduler.ps1",
                 "optimize-services.ps1",
                 "optimize-privacy.ps1",
                 "optimize-performance.ps1",
