86f0bf3546a10e9365dd01ca523904843be12ee5
Plínio Larrubia <plinio2xd@gmail.com>
2023-04-06 12:30:51 -0300

    Don't set NVIDIA services to Manual anymore (#98) - Solves issues with GeForce Experience and Control Panel

--- src/scripts/Optimize-ServicesRunning.ps1
+++ src/scripts/Optimize-ServicesRunning.ps1
@@ -76,9 +76,6 @@ function Optimize-ServicesRunning() {
         "XblGameSave"                    # DEFAULT: Manual    | Xbox Live Game Save
         "XboxGipSvc"                     # DEFAULT: Manual    | Xbox Accessory Management Service
         "XboxNetApiSvc"                  # DEFAULT: Manual    | Xbox Live Networking Service
-        <# NVIDIA services #>
-        "NVDisplay.ContainerLocalSystem" # DEFAULT: Automatic | NVIDIA Display Container LS (NVIDIA Control Panel)
-        "NvContainerLocalSystem"         # DEFAULT: Automatic | NVIDIA LocalSystem Container (GeForce Experience / NVIDIA Telemetry)
         <# Printer services #>
         #"PrintNotify"                   # DEFAULT: Manual    | WARNING! REMOVING WILL TURN PRINTING LESS MANAGEABLE | Printer Extensions and Notifications
         #"Spooler"                       # DEFAULT: Automatic | WARNING! REMOVING WILL DISABLE PRINTING              | Print Spooler
