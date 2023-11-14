fa00311f6829cf2af3d857b6906a4a049b2d70b2
Plínio Larrubia <plinio2xd@gmail.com>
2023-11-14 18:29:58 -0300

    Remove Windows.old folder after cleaning

--- src/scripts/Remove-WindowsOld.ps1
+++ src/scripts/Remove-WindowsOld.ps1
@@ -5,7 +5,8 @@ function Remove-WindowsOld() {
     $TweakType = "Windows.old"
 
     Write-Status -Types "+", $TweakType -Status "Cleaning up Old Windows Installation (Windows.old)..."
-    Start-Process cleanmgr.exe -ArgumentList "/d $env:SystemDrive", "/AUTOCLEAN"
+    Start-Process cleanmgr.exe -ArgumentList "/d $env:SystemDrive", "/AUTOCLEAN" -Wait
+    Remove-ItemVerified -Path "$env:SystemDrive\Windows.old\" -Recurse -Force
 }
 
 $Ask = "Are you sure you want to remove $env:SystemDrive\Windows.old?`nOnly do this AFTER you have moved all your data from there.`n`nList from a few folders:`n- $env:SystemDrive\Windows.old\Users\$env:USERNAME\[...]`n   - AppData`n   - Desktop`n   - Documents`n   - Downloads`n   - Music`n   - Pictures`n   - Videos`n- $env:SystemDrive\Windows.old\ProgramData"
