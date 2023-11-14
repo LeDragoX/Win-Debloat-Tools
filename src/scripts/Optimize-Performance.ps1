f094640ed0f16f23b634c5a59e46fbcda4c83af6
Plínio Larrubia <plinio2xd@gmail.com>
2023-11-14 19:31:12 -0300

    Fix value from mouse hover time - This registry key is using milliseconds unit to define the time

--- src/scripts/Optimize-Performance.ps1
+++ src/scripts/Optimize-Performance.ps1
@@ -101,7 +101,7 @@ function Optimize-Performance() {
     Set-ItemPropertyVerified -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "ClearPageFileAtShutdown" -Type DWord -Value 0 # Default: 0
 
     Write-Status -Types "+", $TweakType -Status "Reducing mouse hover time events to 10ms..."
-    Set-ItemPropertyVerified -Path "HKCU:\Control Panel\Mouse" -Name "MouseHoverTime" -Type String -Value "1000" # Default: 400
+    Set-ItemPropertyVerified -Path "HKCU:\Control Panel\Mouse" -Name "MouseHoverTime" -Type String -Value "10" # Default: "400" (ms)
 
     # Details: https://windowsreport.com/how-to-speed-up-windows-11-animations/ and https://www.tenforums.com/tutorials/97842-change-hungapptimeout-value-windows-10-a.html
     ForEach ($DesktopRegistryPath in @($PathToUsersControlPanelDesktop, $PathToCUControlPanelDesktop)) {
